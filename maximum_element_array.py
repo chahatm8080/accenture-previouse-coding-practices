5494ba7d8df18748d30b6c18069f34d51a47cdec
CHAHAT MISHRA <chahatm8080@gmail.com>
2024-08-09 10:05:45 -0400

    missing_number_in_array previous year questions

--- maximum_element_array.py
+++ maximum_element_array.py
@@ -1,3 +1,4 @@
+# using functions
 # def maximum(arr):
 #   max=arr[0]
 #   for num in arr:
@@ -6,9 +7,11 @@
 #   return max
 # print(maximum([5,6,7,8,10]))
 
+#without using functions 
 arr=[5,6,6,5,54]
 max = arr[0]
 for num in arr:
   if num>max:
     max=num
 print(max)
+
---
+++ missing_number_in_array.py
@@ -0,0 +1,16 @@
+# def missing(arr):
+#   n=len(arr)+1
+#   total_sum=n*(n+1)//2
+#   actual_sum=sum(arr)
+#   return total_sum-actual_sum
+
+
+# print(missing([1,2,3,5,6]))
+
+def missing_num(arr):
+  n=len(arr)+1
+  total_sum=n*(n+1)//2
+  actual_sum=sum(arr)
+  return total_sum-actual_sum
+
+print(missing_num([3,5,4,1,2]))
