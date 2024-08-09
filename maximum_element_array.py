fbc8b08a4ff41fbd671f5524b6fe9ee4f4678a4f
CHAHAT MISHRA <chahatm8080@gmail.com>
2024-08-09 09:45:52 -0400

    without using functions

--- maximum_element_array.py
+++ maximum_element_array.py
@@ -1,7 +1,14 @@
-def maximum(arr):
-  max=arr[0]
-  for num in arr:
-    if num>max:
-      max=num
-  return max
-print(maximum([5,6,7,8,10]))
+# def maximum(arr):
+#   max=arr[0]
+#   for num in arr:
+#     if num>max:
+#       max=num
+#   return max
+# print(maximum([5,6,7,8,10]))
+
+arr=[5,6,6,5,54]
+max = arr[0]
+for num in arr:
+  if num>max:
+    max=num
+print(max)
