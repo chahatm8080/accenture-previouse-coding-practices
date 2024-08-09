d7961b7e9d1fc16dbb632a572f60c8a1f2b84721
CHAHAT MISHRA <chahatm8080@gmail.com>
2024-08-09 14:22:45 -0400

    intersection_two_Arrays accenture

---
+++ intersection_two_arrays.py
@@ -0,0 +1,15 @@
+def intersection(arr1,arr2):
+  i,j=0,0
+  ans=[]
+  while i<len(arr1) and j<len(arr2):
+    if arr1[i]<arr2[j]:
+      i += 1
+    elif arr2[j]<arr1[i]:
+      j += 1
+    else:
+      ans.append(arr1[i])
+      i += 1
+      j += 1
+  return ans
+
+print(intersection([1,2,2,4],[2,2,3,4]))
