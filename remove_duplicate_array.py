bf6176b126f170fe4ff7d20372cb3ac0c12eacae
CHAHAT MISHRA <chahatm8080@gmail.com>
2024-08-16 14:44:10 -0400

    remove duplicate array without using the map manually assingning the unique element and finall getting the result using slice method in python

--- remove_duplicate_array.py
+++ remove_duplicate_array.py
@@ -1,9 +1,21 @@
-def remove_duplicate_array(arr):
-  unique_elements=[]
-  for elem in arr:
-    if elem not in unique_elements:
-      unique_elements.append(elem)
-  return unique_elements
-      
+# def remove_duplicate_array(arr):
+#   unique_elements=[] 
+#   for elem in arr:# tc O(n)
+#     if elem not in unique_elements:
+#       unique_elements.append(elem)
+#   return unique_elements
+# print(remove_duplicate_array([10,10,9,9,8]))#time = O(n) Space = O(n)  map using
 
-print(remove_duplicate_array([10,10,9,9,8]))
+
+
+#Optimized code without using extra space now i am trying to make it big of O(1)
+
+def rm_duplicate(arr):
+  arr.sort()
+  index=0
+  for i in range(1,len(arr)):
+    if arr[i] != arr[index]:
+      index +=1
+      arr[index]=arr[i]
+  return arr[:index+1]
+print(rm_duplicate([10,10,9,9,8,12,11,11]))
