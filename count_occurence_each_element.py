60d1e937163a288bed28c200ffe13bbbb1260a64
CHAHAT MISHRA <chahatm8080@gmail.com>
2024-08-18 18:28:05 -0400

    count occurence of each element in an array without using extra space

--- count_occurence_each_element.py
+++ count_occurence_each_element.py
@@ -1,9 +1,33 @@
-def occurence(arr):
-  count={}
-  for elem in arr:
-    if elem in count:
-      count[elem]+=1
-    else:
-      count[elem]=1
-  return count
-print(occurence([10,10,2,2,1,3]))
+# def occurence(arr):
+#   count={}
+#   for elem in arr:
+#     if elem in count:
+#       count[elem]+=1
+#     else:
+#       count[elem]=1
+#   return count
+# print(occurence([10,10,2,2,1,3]))# tc=O(n) and sc=O(n)
+
+# Counting occurence of each element in an array
+def count_occurrences(arr):
+    arr.sort()  
+    n = len(arr)
+    
+    if n == 0:
+        return
+    
+    current = arr[0]
+    count = 1
+    #[1,2,2,3,10,10]
+    for i in range(1, n):
+        if arr[i] == current:
+            count += 1
+        else:
+            print(f"{current} occurs {count} time")
+            current = arr[i]
+            count = 1
+    
+    print(f"{current} occurs {count} time")# f is formatted string literal which is used to define the variable 
+
+# Example usage
+count_occurrences([10, 10, 2, 2, 1, 3])
---
+++ sum_of_prime_number_one_to_n.py
@@ -0,0 +1,17 @@
+def SumOfPrimes(n):
+  prime=[True]*(n+1)
+  p=2
+  while p*p <=n :
+    if prime[p]==True:
+      i=p*2
+      while i<=n:
+        prime[i]=False
+        i+=p
+    p+=1
+  sum=0
+  for i in range(2, n+1):
+    if(prime[i]):
+      sum +=i
+  return sum
+
+print(SumOfPrimes(10))
